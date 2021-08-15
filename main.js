53ed51d116b491708fe76b481f25c99c29f07d7d
VishwajeethV <spacexblue80@gmail.com>
2021-08-15 15:22:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var nose_x =0;
+var nose_y =0;
+
+
+function preload() {
+    cap_image = loadImage("mask-removebg-preview.png");
+}
+
+function setup() {
+
+    canvas = createCanvas(300,250);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,250);
+    video.hide();
+
+    cposenet = ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',getposes);
+    
+}
+
+function modelLoaded()  {
+
+    console.log("modelLoaded");
+}
+
+function getposes(result) {
+
+    if(result.length>0){
+
+        console.log(result);
+        nose_x = result[0].pose.nose.x;
+        nose_y = result[0].pose.nose.y;
+        console.log(nose_x);
+        console.log(nose_y);
+    }
+}
+
+
+function draw() {
+
+image(video,0,0,300,250);
+fill(130,12,0);
+circle(nose_x,nose_y,20);
+image(cap_image,nose_x-15,nose_y-15,30,30);
+}
+
+function take_snapshot() {
+
+ save("cap_filter.jpg");
+} 
